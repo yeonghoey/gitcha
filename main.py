6f602e9b18ec0f2f5b669d08d2c7594c6c3571d3
Yeongho Kim <yeonghoey@gmail.com>
2016-06-04 17:14:02 +0900

    Initial implementation

--- main.py
+++ main.py
@@ -1,28 +1,47 @@
+import re
+import multiprocessing
+
 from flask import Flask
 from flask import request
 from flask import redirect
 from flask import url_for
 from flask import render_template
 
-import 
+import predictionio
 
 TITLE = 'gitcha'
 
+USER_PATTERN = r'(\w|-)+'
+REPO_PATTERN = r'{user}/(\w|-|\.)+'.format(user=USER_PATTERN)
+
 app = Flask(__name__)
+user_re = re.compile(USER_PATTERN)
+repo_re = re.compile(REPO_PATTERN)
+
+engine_client = predictionio.EngineClient(url="https://localhost:8000")
 
 @app.route('/')
 def index():
     query = request.args.get('query', '')
 
-    output = []
+    output = None
     if query:
-        param = make_query_param(query)
-        output = request_to_engine(param)
+        output = request_to_engine(query)
 
-    return render_template('index.html', title=TITLE)
+    return render_template('index.html', title=TITLE, output=output)
 
-def make_query_param(query):
-    pass
+def request_to_engine(query):
+    if user_re.match(query):
+        rep = engine_client.send_query({"user": query})
+        return rep_to_output(rep)
+    elif repo_re.match(query):
+        rep = engine_client.send_query({"user": query})
+        return rep_to_output(rep)
+    else:
+        return []
+
+def rep_to_output(rep):
+    return [ item_dict['item'] for item_dict in rep['itemScores']]
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
